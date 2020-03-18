760300d3e7d162f31ccf42bc6f60539d7965063c
Guillem96 <guillem.orellana@gmail.com>
2020-03-18 18:43:22 +0100

    Initial commit

---
+++ ar/data/download.py
@@ -0,0 +1,234 @@
+import uuid
+import json
+import shutil
+import argparse
+import subprocess
+from pathlib import Path
+from collections import OrderedDict
+from typing import Union, Mapping, Any
+
+from joblib import delayed
+from joblib import Parallel
+
+import pandas as pd
+
+
+def create_video_folders(
+    dataset: pd.DataFrame, 
+    output_dir: Union[str, Path], 
+    tmp_dir: Union[str, Path]) -> Union[Mapping[str, Path], Path]:
+
+    """Creates a directory for each label name in the dataset."""
+    if 'label-name' not in dataset.columns:
+        this_dir = Path(output_dir, 'test') 
+        this_dir.mkdir(exist_ok=True)
+        return this_dir
+    
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+    
+    tmp_dir = Path(tmp_dir)
+    tmp_dir.mkdir(exist_ok=True)
+
+    label_to_dir = {}
+    for label_name in dataset['label-name'].unique():
+        this_dir = output_dir / label_name
+        this_dir.mkdir(exist_ok=True)
+        label_to_dir[label_name] = this_dir
+
+    return label_to_dir
+
+
+def construct_video_filename(row: Mapping[str, Any], 
+                             label_to_dir: Mapping[str, Path]) -> Path:
+    """Given a dataset row, this function constructs the
+       output filename for a given video.
+    """
+    v_id, start_t, end_t = row['video-id'], row['start-time'], row['end-time']
+    basename = f'{v_id}_{start_t:06d}_{end_t:06d}.mp4'
+
+    if not isinstance(label_to_dir, dict):
+        dirname = label_to_dir
+    else:
+        dirname = label_to_dir[row['label-name']]
+
+    return dirname / basename
+
+
+def download_clip(video_identifier: str, 
+                  output_filename: Union[str, Path],
+                  start_time: int, end_time: int,
+                  tmp_dir: str = '/tmp/kinetics',
+                  num_attempts: int = 5,
+                  url_base: str = 'https://www.youtube.com/watch?v='):
+    """Download a video from youtube if exists and is not blocked.
+
+    Parameters
+    ----------
+    video_identifier: str
+        Unique YouTube video identifier (11 characters)
+    output_filename: str
+        File path where the video will be stored.
+    start_time: float
+        Indicates the begining time in seconds from where the video
+        will be trimmed.
+    end_time: float
+        Indicates the ending time in seconds of the trimmed video.
+    """
+    # Defensive argument checking.
+    assert isinstance(video_identifier, str), 'video_identifier must be string'
+    assert isinstance(output_filename, (str, Path)), 'output_filename must be path'
+    assert len(video_identifier) == 11, 'video_identifier must have length 11'
+
+    status = False
+    # Construct command line for getting the direct video link.
+    tmp_filename = Path(tmp_dir, '%s.%%(ext)s' % uuid.uuid4())
+    url = url_base + video_identifier
+    command = ['youtube-dl',
+               '--quiet', '--no-warnings',
+               '-f', 'mp4',
+               '-o', f'"{str(tmp_filename)}"',
+               f'"{url}"']
+    command = ' '.join(command)
+    attempts = 0
+    while True:
+        try:
+            output = subprocess.check_output(command, shell=True,
+                                             stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            attempts += 1
+            if attempts == num_attempts:
+                return status, err.output
+        else:
+            break
+    
+    # As we don't know the extension we have to look for the file using its name
+    tmp_filename = list(tmp_filename.parent.glob(tmp_filename.stem + '*'))[0]
+
+    # Construct command to trim the videos (ffmpeg required).
+    command = ['ffmpeg',
+               '-i', f'"{str(tmp_filename)}"',
+               '-ss', str(start_time),
+               '-t', str(end_time - start_time),
+               '-c:v', 'libx264', '-c:a', 'copy',
+               '-threads', '1',
+               '-loglevel', 'panic',
+               f'"{str(output_filename)}"']
+    command = ' '.join(command)
+    try:
+        output = subprocess.check_output(command, shell=True,
+                                         stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as err:
+        return status, err.output
+
+    # Check if the video was successfully saved.
+    status = tmp_filename.exists()
+    tmp_filename.unlink(missing_ok=True)
+    return status, 'Downloaded'
+
+
+def download_clip_wrapper(row: Mapping[str, Any], 
+                          label_to_dir: Mapping[str, Path],  
+                          tmp_dir: Union[str, Path]):
+    """Wrapper for parallel processing purposes."""
+    output_filename = construct_video_filename(
+        row, label_to_dir)
+
+    clip_id = output_filename.stem
+    if output_filename.exists():
+        status = tuple([clip_id, True, 'Exists'])
+        return status
+
+    downloaded, log = download_clip(row['video-id'], output_filename,
+                                    row['start-time'], row['end-time'],
+                                    tmp_dir=tmp_dir)
+    status = tuple([clip_id, downloaded, log])
+    return status
+
+
+def parse_kinetics_annotations(input_csv: str, 
+                               ignore_is_cc: bool = False) -> pd.DataFrame:
+    """Returns a parsed DataFrame.
+
+    arguments:
+    ---------
+    input_csv: str
+        Path to CSV file containing the following columns:
+          'YouTube Identifier,Start time,End time,Class label'
+
+    returns:
+    -------
+    dataset: DataFrame
+        Pandas with the following columns:
+            'video-id', 'start-time', 'end-time', 'label-name'
+    """
+    df = pd.read_csv(input_csv)
+    if 'youtube_id' in df.columns:
+        columns = OrderedDict([
+            ('youtube_id', 'video-id'),
+            ('time_start', 'start-time'),
+            ('time_end', 'end-time'),
+            ('label', 'label-name')])
+        df.rename(columns=columns, inplace=True)
+        if ignore_is_cc:
+            df = df.loc[:, df.columns.tolist()[:-1]]
+    return df
+
+
+def main(input_csv: str, 
+         output_dir: str,
+         num_jobs: int = 24, 
+         tmp_dir: str = '/tmp/kinetics',
+         drop_duplicates: bool = False):
+
+    # Reading and parsing Kinetics.
+    dataset = parse_kinetics_annotations(input_csv)
+    # if os.path.isfile(drop_duplicates):
+    #     print('Attempt to remove duplicates')
+    #     old_dataset = parse_kinetics_annotations(drop_duplicates,
+    #                                              ignore_is_cc=True)
+    #     df = pd.concat([dataset, old_dataset], axis=0, ignore_index=True)
+    #     df.drop_duplicates(inplace=True, keep=False)
+    #     print(dataset.shape, old_dataset.shape)
+    #     dataset = df
+    #     print(dataset.shape)
+
+    # Creates folders where videos will be saved later.
+    label_to_dir = create_video_folders(dataset, output_dir, tmp_dir)
+
+    # Download all clips.
+    if num_jobs == 1:
+        status_lst = []
+        for i, row in dataset.iterrows():
+            status_lst.append(download_clip_wrapper(row, label_to_dir,
+                                                    tmp_dir))
+    else:
+        delayed_download_clip = delayed(download_clip_wrapper)
+        parallel_job = Parallel(n_jobs=num_jobs)
+        generator = (delayed_download_clip(
+            row, label_to_dir, tmp_dir) 
+            for i, row in dataset.iterrows())
+        status_lst = parallel_job(generator) 
+
+    # Clean tmp dir.
+    shutil.rmtree(tmp_dir)
+
+    # Save download report.
+    with open('download_report.json', 'w') as f:
+        f.write(json.dumps(status_lst))
+
+
+if __name__ == '__main__':
+    description = 'Helper script for downloading and trimming kinetics videos.'
+    p = argparse.ArgumentParser(description=description)
+    p.add_argument('input_csv', type=str,
+                   help=('CSV file containing the following format: '
+                         'YouTube Identifier,Start time,End time,Class label'))
+    p.add_argument('output_dir', type=str,
+                   help='Output directory where videos will be saved.')
+    p.add_argument('-n', '--num-jobs', type=int, default=24)
+    p.add_argument('-t', '--tmp-dir', type=str, default='/tmp/kinetics')
+    p.add_argument('--drop-duplicates', type=str, default='non-existent',
+                   help='Unavailable at the moment')
+                   # help='CSV file of the previous version of Kinetics.')
+    main(**vars(p.parse_args()))
