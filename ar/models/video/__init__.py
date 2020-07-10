57dee4ae997d7876bda420b896eabbcf2cc0b622
Orellana Trullols <email@example.com>
2020-07-10 17:21:32 +0200

    New clips sampling method

--- ar/models/video/__init__.py
+++ ar/models/video/__init__.py
@@ -2,4 +2,5 @@ from .models import LRCN
 from .models import LRCNWithAudio
 from .models import FstCN
 
+from .clip_samplers import lrcn_sampling
 from .clip_samplers import uniform_sampling
--- ar/models/video/clip_samplers.py
+++ ar/models/video/clip_samplers.py
@@ -5,7 +5,7 @@ import torch
 
 def uniform_sampling(video: torch.Tensor, 
                      clips_len: int, 
-                     n_clips: int = 3,
+                     n_clips: int = 10,
                      overlap: bool = True) -> List[torch.Tensor]:
     """
     Uniformly samples `n_clips` from a video. The clips samples by default can 
@@ -38,3 +38,46 @@ def uniform_sampling(video: torch.Tensor,
     
     clips = [video[i: i + clips_len] for i in indices]
     return clips
+
+
+def lrcn_sampling(video: torch.Tensor, 
+                  clips_len: int, 
+                  n_clips: int = 16,
+                  stride: int = 8) -> List[torch.Tensor]:
+    """
+    Sample clips as described in LRCN paper. 
+    Citation: At test time, we extract 16 frame clips with a stride of 8 frames
+    from each video.
+
+    If n_clips is larger than the total clips available in video the resulting
+    list will have at most the clips available in the video.
+
+    Parameters
+    ----------
+    video: torch.Tensor
+        Tensor of shape [T, H, W, C] or [T, C, H, W]
+    clips_len: int
+        Length of the sampled clips in frames
+    n_clips: int, default 16
+        Number of clips to sample
+    stride: int, default 8
+        Distance in frames 
+    Returns
+    -------
+    List[torch.Tensor]
+        List of tensors containing `n_clips` of shape [clips_len, H, W, C]
+    """
+    n_video_frames = video.size(0)
+    max_clips = n_video_frames // (clips_len + stride)
+
+    if n_clips > max_clips:
+        n_clips = max_clips
+
+    start_idx = torch.randint(0, 
+        high=(n_video_frames - (clips_len + stride) * n_clips) + 1,
+        size=(1,)).item()
+    
+    clips_start_idx = torch.arange(
+        start_idx, n_video_frames,  stride + clips_len)[:n_clips]
+    
+    return [video[i: i + clips_len] for i in clips_start_idx.tolist()]
